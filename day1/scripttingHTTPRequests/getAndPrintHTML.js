7f4b83e03fc61e2039f4b0eeba59a8d77cb9e9b9
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-03 15:51:26 +0000

    fixed getAndPrintHTML.js to actually print out the response after joining the 'chunks'

--- day1/scripttingHTTPRequests/getAndPrintHTML.js
+++ day1/scripttingHTTPRequests/getAndPrintHTML.js
@@ -6,25 +6,34 @@ var request = require('https');
 function getAndPrintHTML() {
     // preference to how the data chunks are stored?
   var chunks = [];
+  var result;
 
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
     // https request
-    request.get(requestOptions, function (res) {
+    result = request.get(requestOptions, function (res) {
+      var returnedString;
+
       res.setEncoding('utf8');
 
       res.on('data', function (data) {
         console.log('Chunk Received. Length: ', data.length + '\n');
 
         //* buffer data:
-        chunks.push(data);
+        chunks.push(data.toString());
       })
 
-      console.log(chunks.join(" "));
-
+      return returnedString = res.on('end', function () {
+          console.log('Response is complete...');
+        var combineChunksResult = chunks.join(' ');
+        return combineChunksResult;
+      });      
     });
 
-
+  console.log(result);
 }
+
+
+getAndPrintHTML();
